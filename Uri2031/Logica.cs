61b8502c5b8485fae4447db97d682a11d55b4c0e
Henrique Greghi <henriquevsg@gmail.com>
2022-08-29 16:48:26 -0300

    jogo

---
+++ Uri2031/Logica.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Uri2031
+{
+    class Logica
+    {
+        private string pedra { get; set; }
+        private string papel { get; set; }
+        private string ataque { get; set; }
+        private string Jogador1 { get; set; }
+        private string Jogador2 { get; set; }
+        private string  jogada { get; set; }
+
+
+        public Logica(string jogador1, string jogador2)
+        {
+            Jogador1 = jogador1;
+            Jogador2 = jogador2;
+        }
+
+        
+        public void Jogatadas()
+        {
+            if (Jogador1 == "ataque" && Jogador2 == "ataque" || Jogador1 == "pedra" && Jogador2 == "pedra")
+            {
+              jogada = " Todos perdem";
+            }
+            else if (Jogador1 == "papel" && Jogador2 == "papel")
+            {
+                jogada = " Todos ganham";
+            }
+            else if (Jogador1 == "ataque" && Jogador2 == "pedra" || Jogador1 == "pedra" && Jogador2 == "papel" || Jogador1 == "ataque" && Jogador2 == "papel")
+            {
+                jogada = " Jogador 1 venceu";
+            }
+            else if (Jogador2 == "ataque" && Jogador1 == "pedra" || Jogador2 == "pedra" && Jogador1 == "papel" || Jogador2 == "ataque" && Jogador1 == "papel") 
+            {
+                jogada = " Jogador 2 venceu";
+            }
+            else
+            {
+                jogada = " Erro ao escrever a jogada";
+            }
+
+        }
+        public string Imprimir()
+        {
+            return jogada;
+        }
+    }
+}
---
+++ Uri2031/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Uri2031
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Entre com quantos testes pretende fazer :");
+            int testes = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i < testes +1 ; i++)
+            {
+                Console.WriteLine("Escolha entre: ataque, pedra ou papel");
+                Console.WriteLine("Jogador 1 :");
+                string jogador1 = Console.ReadLine();
+                Console.WriteLine("Jogador 2 :");
+                string jogador2 = Console.ReadLine();
+
+                Logica jogo = new Logica(jogador1,jogador2);
+
+                jogo.Jogatadas();
+                
+
+                Console.WriteLine($"Teste #{i} =" + jogo.Imprimir());
+                Console.WriteLine();
+            }
+        }
+    }
+}
